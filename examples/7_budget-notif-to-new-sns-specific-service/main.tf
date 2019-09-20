913034fc37fe7600da165d44e5013523f39b7a0c
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2019-09-20 12:48:53 -0300

    BBL-122 updating examples

--- examples/7_budget-notif-to-new-sns-specific-service/main.tf
+++ examples/7_budget-notif-to-new-sns-specific-service/main.tf
@@ -1,14 +1,13 @@
 module "cost_mgmt_notif" {
   source = "../../../terraform-aws-cost-budget"
 
-  aws_env               = "${var.aws_profile}"
-  currency              = "USD"
-  limit_amount          = 500
-  time_unit             = "MONTHLY"
-  time_period_start     = "2019-01-01_00:00"
-  cost_filters_service  = "Amazon Elastic Compute Cloud - Compute"
-  aws_sns_account_id    = "111111111111"
-
+  aws_env              = "${var.aws_profile}"
+  currency             = "USD"
+  limit_amount         = 500
+  time_unit            = "MONTHLY"
+  time_period_start    = "2019-01-01_00:00"
+  cost_filters_service = "Amazon Elastic Compute Cloud - Compute"
+  aws_sns_account_id   = "111111111111"
 }
 
 output "sns_topic" {
