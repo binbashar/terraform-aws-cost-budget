34cb99f1ca6ac3067f5e580b8f146739c8da5939
Luis M. Gallardo D <lgallard@gmail.com>
2021-10-27 02:39:01 -0300

    Fix subscriber_sns_topic_arns paramter

--- main.tf
+++ main.tf
@@ -79,7 +79,7 @@ resource "aws_budgets_budget" "budget_notifification" {
     threshold                 = lookup(local.notification, "threshold")
     threshold_type            = lookup(local.notification, "threshold_type")
     notification_type         = lookup(local.notification, "notification_type")
-    subscriber_sns_topic_arns = each.value
+    subscriber_sns_topic_arns = [each.value]
     #  }
   }
 
