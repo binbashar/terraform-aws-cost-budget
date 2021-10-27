9498c1253a5c0ddbfedd13ae8c6a7c82e57c1e7b
Luis M. Gallardo D <lgallard@gmail.com>
2021-10-27 01:29:14 -0300

    Change output resources

--- outputs.tf
+++ outputs.tf
@@ -12,37 +12,37 @@
 #
 output "name" {
   description = "% Threshold when the notification should be sent."
-  value       = aws_budgets_budget.budget_notifification["name"]
+  value       = aws_budgets_budget.budget_notifification.name
 }
 
 output "limit_amount" {
   description = "Monthly billing threshold in dollars"
-  value       = aws_budgets_budget.budget_notifification["limit_amount"]
+  value       = aws_budgets_budget.budget_notifification.limit_amount
 }
 
 output "currency" {
   description = "Billing currency eg: dollars"
-  value       = aws_budgets_budget.budget_notifification["limit_unit"]
+  value       = aws_budgets_budget.budget_notifification.limit_unit
 }
 
 output "time_period_start" {
   description = "Time to start."
-  value       = aws_budgets_budget.budget_notifification["time_period_start"]
+  value       = aws_budgets_budget.budget_notifification.time_period_start
 }
 
 output "time_period_end" {
   description = "Time to end."
-  value       = aws_budgets_budget.budget_notifification["time_period_end"]
+  value       = aws_budgets_budget.budget_notifification.time_period_end
 }
 
 output "time_unit" {
   description = "The length of time until a budget resets the actual and forecasted spend. Valid values: MONTHLY, QUARTERLY, ANNUALLY."
-  value       = aws_budgets_budget.budget_notifification["time_unit"]
+  value       = aws_budgets_budget.budget_notifification.time_unit
 }
 
 output "cost_filters_service" {
   description = "Budget service cost filter, eg: Amazon Elastic Compute Cloud - Compute / Amazon Relational Database Service / Amazon Redshift / Amazon ElastiCache/ Amazon Elasticsearch Service"
-  value       = aws_budgets_budget.budget_notifification["cost_filters"]["Service"]
+  value       = aws_budgets_budget.budget_notifification.cost_filters["Service"]
 }
 
 #
