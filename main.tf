dd6d2be0e5187bccbd04c3e60de7bbf531b3c936
Luis M. Gallardo D <lgallard@gmail.com>
2021-10-27 01:46:59 -0300

    Add logic got budget name

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ locals {
 
   budget = {
 
-    name              = "budget-${var.cost_filters_service}-${var.time_unit}-${var.aws_env}"
+    name              = var.cost_filters_service == null ? "budget-${var.time_unit}-${var.aws_env}" : "budget-${var.cost_filters_service}-${var.time_unit}-${var.aws_env}"
     budget_type       = "COST"
     limit_amount      = var.limit_amount
     limit_unit        = var.currency
